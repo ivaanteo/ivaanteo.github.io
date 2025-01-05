54e2ba0fe663d4f79b428ddcfa7437122b00c014
Ivan <ivanteozhiming@gmail.com>
2025-01-05 17:07:36 +0800

    remove unused icon

--- ivaanteo/src/App.tsx
+++ ivaanteo/src/App.tsx
@@ -4,7 +4,6 @@ function App() {
 
   return (
     <>
-
       <h1>👋 Welcome!</h1>
       <p>This will be a place where I share about myself, and write articles on iOS, crypto & defi, and life in general.</p>
       <p>Stay tuned!</p>
